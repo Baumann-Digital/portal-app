4e5a629b26fb6d43f3bfc76a02c7f2c917423f6c
Dennis Ried <ried-musikforschung@mail.de>
2020-07-18 15:07:17 +0200

    add img and mail at assets to ignore (local patch)

--- resources/js/assets/mail/contact_me.php
+++ resources/js/assets/mail/contact_me.php
@@ -2,7 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
+   empty($_POST['subject'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,14 +12,14 @@ if(empty($_POST['name'])      ||
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'ried-musikforschung@mail.de'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Baumann-Digital – Anfrage von $name";
+$email_body = "Es wurde eine Anfrage über das Portal Baumann-Digital gestellt.\n\n"."Name: $name\n\nEmail: $email_address\n\nBetreff: $subject\n\nNachricht:\n$message";
+$headers = "From: dennisried@mail.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
