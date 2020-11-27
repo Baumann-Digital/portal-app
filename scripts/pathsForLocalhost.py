bb8425ce95d582ad803d0469388c435758e67fe5
Dennis Ried <ried-musikforschung@mail.de>
2020-11-27 16:42:24 +0100

    insert path resolver .py for localhost

---
+++ scripts/pathsForLocalhost.py
@@ -0,0 +1,19 @@
+
+import os, fnmatch
+
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
+
+collection = "/Users/dried/Repositories/Baudi/portal-app"
+
+old = 'https://baumann-digital.de'
+new = 'http://localhost:8080/exist/apps/baudiApp'
+
+findReplace(collection, old, new, "*.*")
