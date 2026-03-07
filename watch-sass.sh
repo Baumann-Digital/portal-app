b08de8b84ed11decb6e08f5ec2e14c7f888e4a97
Dennis Ried <dennis.ried@musikwiss.uni-halle.de>
2026-03-07 15:55:50 +0100

    update and fixing building sass

--- watch-sass.sh
+++ watch-sass.sh
@@ -4,8 +4,8 @@
 #!/bin/bash
 
 echo "Starting SASS watch mode..."
-echo "Compiling main.scss → main.css"
+echo "Compiling main.scss → theme.css"
 echo "Press Ctrl+C to stop"
 echo ""
 
-sass --watch resources/sass/main.scss:resources/css/main.css --style expanded --source-map
+sass --watch resources/sass/main.scss:resources/css/theme.css --style expanded --source-map
